a295f1dfceb5e21211bd48ce9bc0b2a304ae6790
sendz <sendzation@gmail.com>
2014-07-19 00:40:38 +0700

    add error reporting

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set('display_errors','On'); ini_set('error_reporting','E_ALL | E_STRICT'); error_reporting(E_ALL);
+
 if (version_compare($ver = PHP_VERSION, $req = '5.4.0', '<')) {
 	exit(sprintf('You are running PHP %s, but Pagekit needs at least <strong>PHP %s</strong> to run.', $ver, $req));
 }
